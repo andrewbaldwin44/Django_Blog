e60053e1825b4fc26db6994a3e1ebeeaab67b5da
Andrew Baldwin <andrew.baldwin44@gmail.com>
2023-01-22 16:45:14 -0500

    Remove dotenv

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -14,12 +14,9 @@ import os
 import json
 import dj_database_url
 
-from dotenv import load_dotenv
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-load_dotenv(BASE_DIR / '.env')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
